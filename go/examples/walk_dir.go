26204fea6a7b85f216eeb5d3a12ff9cc2c617a31
Connor Vanderhook <connorvhook@gmail.com>
2019-08-18 21:50:37 +1000

    feat: fun in the sun

---
+++ go/examples/walk_dir.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var depth int
+var trackedFileTypes = []string{".go", ".js", ".md", ".py", ".es", ".java", ".cpp", ".c", ".png", ".jpg", ".jpeg"}
+
+func init() {
+	flag.IntVar(&depth, "depth", 0, "how deep is your walk...")
+	flag.Parse()
+}
+
+func main() {
+	if depth == 0 {
+		walkCurDir("*")
+	} else {
+		walkRecursive(".")
+	}
+}
+
+func walkCurDir(dir string) {
+	for fi := range genDirFileInfo(dir) {
+		if fi.IsDir() {
+			printDir(fi)
+		} else {
+			fmt.Println(fi.Name())
+		}
+	}
+}
+
+func genDirFileInfo(dir string) chan os.FileInfo {
+	files, err := filepath.Glob(dir)
+	if err != nil {
+		log.Print(err)
+	}
+
+	fChan := make(chan os.FileInfo)
+	// generator anon func
+	go func() {
+		for _, fName := range files {
+			file, err := os.Open(fName)
+			fi, err := file.Stat()
+			if err != nil {
+				log.Print(err)
+			}
+			fChan <- fi
+		}
+		close(fChan)
+	}()
+
+	return fChan
+}
+
+func walkRecursive(dir string) {
+	filepath.Walk(dir, walkProjFunc)
+}
+
+func walkProjFunc(fp string, fi os.FileInfo, err error) error {
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+
+	// what are the 2 letter dirs?
+	if fi.IsDir() && len(fi.Name()) > 2 {
+		printDir(fi)
+		return nil
+	}
+
+	for _, fType := range trackedFileTypes {
+		match, err := filepath.Match("*"+fType, fi.Name())
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+		if match {
+			// fmt.Println(fp)
+			_, file := filepath.Split(fp)
+			fmt.Println(file)
+		}
+	}
+
+	return nil
+}
+
+func printDir(fi os.FileInfo) {
+	fmt.Println()
+	fmt.Println(fi.Name())
+	fmt.Println("-----------------------")
+}

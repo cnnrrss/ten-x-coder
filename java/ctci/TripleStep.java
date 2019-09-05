ebbb684f38be01ce5a6e3fc8611376737c8f9cc6
Connor Vanderhook <connorvhook@gmail.com>
2019-09-05 22:42:34 +1000

    feat: ctci, recursion

---
+++ java/ctci/TripleStep.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class TripleStep {
+
+    public TripleStep() {}
+
+    public int countWays(int n) {
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        return countWays(n, memo);
+    }
+
+    public int countWays(int n, int[] memo) {
+        if (n < 0) {
+            return 0;
+        } else if (n == 0) {
+            return 1;
+        } else if (memo[n] > -1) {
+            return memo[n];
+        } else {
+            memo[n] = countWays(n - 1, memo) + countWays(n - 2, memo) + countWays(n - 3, memo);
+            return memo[n];
+        }
+    }
+
+    public static void main(String[] args) {
+        TripleStep ts = new TripleStep();
+        Integer answer = -1;
+        answer = ts.countWays(5);
+        System.out.println(answer);
+    }
+}
